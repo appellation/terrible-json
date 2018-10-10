7a5c9f899cb83ffedb13039095d370b90b88cb31
Will Nelson <will@pleb.xyz>
2018-10-09 18:46:24 -0700

    upgrades

--- index.js
+++ index.js
@@ -1,50 +1,20 @@
-const fs = require('fs');
-const p = require('path');
+const fsn = require('fs-nextra');
+const sem = require('semaphore')(1);
 const lockfile = require('proper-lockfile');
-const queue = require('queue');
 
-const queues = new Map();
+const take = () => new Promise(r => sem.take(r));
 
-module.exports = (file, data, options = { encoding: 'utf8' }) => {
-    const path = p.resolve(file);
+module.exports = async (file, data, options) => {
+	await take();
+	const release = await lockfile.lock(file);
 
-    return new Promise((resolve, reject) => {
-        addToQueue(path, cb => {
-            lockfile.lock(path, (err, release) => {
-                if(err) return reject(err);
+	async function complete(d) {
+		await release();
+		sem.leave();
+		return d;
+	}
 
-                // read/write depending on arguments
-                if(typeof data !== 'undefined')
-                    fs.writeFile(path, (typeof data === 'string' || data instanceof Buffer) ? data : JSON.stringify(data), options, complete);
-                else
-                    fs.readFile(path, options, complete);
-
-                function complete(err, data) {
-                    release();
-                    cb();
-
-                    if(err) reject(err);
-                    else resolve(data);
-                }
-            });
-        });
-    });
+	// read/write depending on arguments
+	if (typeof data === 'undefined') return fsn.readJSON(file, options).then(complete);
+	return fsn.writeJSON(file, data, options).then(complete);
 };
-
-function addToQueue(path, func) {
-    let q;
-    if(queues.has(path))   {
-        q = queues.get(path);
-        q.push(func);
-    }   else    {
-        q = queue({
-            concurrency: 1
-        });
-        q.push(func);
-        queues.set(path, q);
-        q.start();
-
-        q.once('success', () => queues.delete(path));
-        q.once('error', () => queues.delete(path));
-    }
-}
--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 const badJSON = require('../index');
-badJSON('./test/bad.json', {lol: 'no'});
-badJSON('./test/bad.json', {lol: 'maybe'});
+badJSON('./test/bad.json', { lol: 'no' });
+badJSON('./test/bad.json', { lol: 'maybe' });
 badJSON('./test/bad.json').then(console.log);
-badJSON('./test/bad.json', {lmao: 'lol'});
+badJSON('./test/bad.json', { lmao: 'lol' });
 badJSON('./test/bad.json').then(console.log);
